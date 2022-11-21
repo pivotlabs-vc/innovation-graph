978c7df40f2a526567b0be7d921af0000278d91c
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-11-21 21:13:08 +0000

    Added DASA challenges

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-const version = "0.4.4";
+const version = "0.4.5";
 
 const provider = new gcp.Provider(
     "gcp",
